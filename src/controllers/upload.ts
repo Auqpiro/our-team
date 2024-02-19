fc863ef13b128e9655a2b977b87b778d150c87bc
Auqpiro <zzoomzoone@gmail.com>
2024-02-19 17:37:05 -0300

    split upload router with upload controller

---
+++ src/controllers/upload.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { imagesStorageRef } from "../storages/firebase";
+import User from "@models/user";
+const uploadUserIcon = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { file } = req;
+  if (!file) {
+    res.status(400).send({ error: "No file uploaded" });
+    return;
+  }
+  const spaceRef = ref(imagesStorageRef, `${id}/${Date.now()}`);
+  const metadata = {
+    contentType: file.mimetype,
+  };
+  uploadBytes(spaceRef, file.buffer, metadata)
+    .then(() => getDownloadURL(spaceRef))
+    .then((url) => User.findByIdAndUpdate(id, { icon: url }))
+    .then(() => res.status(200).send())
+    .catch((error) => {
+      res.status(500).send({ error });
+    });
+};
+export { uploadUserIcon };
--- src/routers/upload.ts
+++ src/routers/upload.ts
@@ -1,34 +1,13 @@
-import express, { Request, Response } from "express";
-import jwt, { Secret } from "jsonwebtoken";
+import express from "express";
 import multer, { memoryStorage } from "multer";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { imagesStorageRef } from "../storages/firebase";
-import User from "@models/user";
 import { jwtAuthCookiesMiddleware } from "@middlewares/auth";
+import { uploadUserIcon } from "@controllers/upload";
 const upload = multer({ storage: memoryStorage() });
 const router = express.Router();
 router.post(
   "/:id/icon",
   upload.single("icon"),
   jwtAuthCookiesMiddleware,
-  (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { file } = req;
-    if (!file) {
-      res.status(400).send({ error: "No file uploaded" });
-      return;
-    }
-    const spaceRef = ref(imagesStorageRef, `${id}/${Date.now()}`);
-    const metadata = {
-      contentType: file.mimetype,
-    };
-    uploadBytes(spaceRef, file.buffer, metadata)
-      .then(() => getDownloadURL(spaceRef))
-      .then((url) => User.findByIdAndUpdate(id, { icon: url }))
-      .then(() => res.status(200).send())
-      .catch((error) => {
-        res.status(500).send({ error });
-      });
-  }
+  uploadUserIcon
 );
 export default router;
